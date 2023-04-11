6392c67f40e8b757810d4e4758e7f9af0629afb5
dgomezde83 <dgomezde83@gmail.com>
2023-04-11 15:51:00 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,23 +34,5 @@ if ! dpkg -s cmake > /dev/null; then
   echo "CMake not installed, installing..."
   sudo apt install cmake || sudo snap install cmake --classic || exit 1
 fi
-cmake . || exit 1
-
-echo "Building solution..."
-cmake --build . || exit 1
-
-echo "Installing sdk..."
-
-sudo cp -r "$CURRENT_DIR/include/" "$INCLUDE_PATH/erdcpp"
-if [[ $? -ne 0 ]]; then
-  echo "Could not copy include headers from $CURRENT_DIR/include/ to $INCLUDE_PATH/erdcpp"
-  exit 1
-fi
-
-sudo cp "$CURRENT_DIR/src/libsrc.so" "$LIB_PATH"
-if [[ $? -ne 0 ]]; then
-  echo "Could not copy generated shared library from $CURRENT_DIR/src/libsrc.so to $LIB_PATH"
-  exit 1
-fi
 
 echo "Finished successfully !"
